9be0ca0dafa82261f4470b7863d1c3b528a174d6
Overdose <80890008+chanyut12@users.noreply.github.com>
2024-03-01 16:34:21 +0700

    Delete week8/StudentTest.java

--- week8/StudentTest.java
+++
@@ -1,9 +0,0 @@
-package week8;
-import week6.Student;
-
-public class StudentTest {
-    public static void main(String[] args) {
-        Student student = new Student("chanyut", "lisawat", 70, 74, 99);
-        System.out.println("The average score of chanyut is: " + student.getAverage());
-    }
-}
