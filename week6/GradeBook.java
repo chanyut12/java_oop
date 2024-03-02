b14bb7bfbe0ca73777d59d8dfe93717edf0d58d7
Overdose <80890008+chanyut12@users.noreply.github.com>
2024-03-02 14:10:39 +0700

    Add files via upload

---
+++ week6/GradeBook.java
@@ -0,0 +1,29 @@
+//Lab 2: GradeBook.java 
+//GradeBook class with a constructor to initialize the course name. 
+public class GradeBook 
+{ 
+    private String courseName; // course name for this GradeBook 
+    // constructor initializes courseName with String supplied as argument 
+    public GradeBook( String name )  
+    {   
+    courseName = name; // initializes courseName  
+    } // end constructor  
+    // method to set the course name 
+    public void setCourseName( String name ) 
+    { 
+    courseName = name; // store the course name 
+    } // end method setCourseName 
+  
+    // method to retrieve the course name 
+    public String getCourseName() 
+    { 
+    return courseName; 
+ }  // end method getCourseName 
+  
+    // display a welcome message to the GradeBook user 
+    public void displayMessage() 
+    { 
+    System.out.printf( "Welcome to the grade book for\n%s!\n",  
+    getCourseName() ); 
+    } // end method displayMessage 
+} // end class GradeBook
---
+++ week6/GradeBookTest.java
@@ -0,0 +1,8 @@
+public class GradeBookTest {
+    //main method begins program execution
+    public static void main(String[] args) {
+        //create GradeBook object 
+        GradeBook gradeBook1 = new GradeBook("chanyut");
+        gradeBook1.displayMessage();
+    }
+}
